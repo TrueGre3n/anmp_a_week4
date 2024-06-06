51ce2c2c6438a492fbe7771dd14013db4d0d809e
TrueGre3n <oeyraymon4@gmail.com>
2024-06-06 14:12:31 +0700

    W11

---
+++ Works/W4App/app/src/main/java/com/ubaya/studentapp/view/Interface.kt
@@ -0,0 +1,7 @@
+package com.ubaya.studentapp.view
+
+import android.view.View
+
+interface ButtonDetailClickListener{
+    fun onButtonDetailClick(v: View)
+}
--- Works/W4App/app/src/main/java/com/ubaya/studentapp/view/StudentListAdapter.kt
+++ Works/W4App/app/src/main/java/com/ubaya/studentapp/view/StudentListAdapter.kt
@@ -1,13 +1,14 @@
 package com.ubaya.studentapp.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ubaya.studentapp.databinding.StudentListItemBinding
 import com.ubaya.studentapp.model.Student
 
-class StudentListAdapter(val studentList:ArrayList<Student>): RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>()
+class StudentListAdapter(val studentList:ArrayList<Student>): RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>(),ButtonDetailClickListener
 {
     class StudentViewHolder(var binding:StudentListItemBinding):RecyclerView.ViewHolder(binding.root)
 
@@ -22,12 +23,14 @@ class StudentListAdapter(val studentList:ArrayList<Student>): RecyclerView.Adapt
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        holder.binding.txtId.text = studentList[position].id
-        holder.binding.txtName.text = studentList[position].name
-        holder.binding.btnDetail.setOnClickListener{
-            val action = StudentListFragmentDirections.actionStudentDetailFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
+//        holder.binding.txtId.text = studentList[position].id
+//        holder.binding.txtName.text = studentList[position].name
+//        holder.binding.btnDetail.setOnClickListener{
+//            val action = StudentListFragmentDirections.actionStudentDetailFragment()
+//            Navigation.findNavController(it).navigate(action)
+//        }
+        holder.binding.student = studentList[position]
+        holder.binding.listener = this
     }
 
     fun updateStudentList(newStudentList:ArrayList<Student>){
@@ -35,5 +38,10 @@ class StudentListAdapter(val studentList:ArrayList<Student>): RecyclerView.Adapt
         studentList.addAll(newStudentList)
         notifyDataSetChanged()
 
+    }
+
+    override fun onButtonDetailClick(v: View) {
+        val action = StudentListFragmentDirections.actionStudentDetailFragment(v.tag.toString())
+        Navigation.findNavController(v).navigate(action)
     }
 }
